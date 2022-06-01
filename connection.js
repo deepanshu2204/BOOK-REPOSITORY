df9b96ed447126487f177f6edc0feade6f386f8b
Deepanshu Agrawal <deepanshu.a@lsq.net>
2022-06-01 16:11:08 +0530

    deepanshu's commit

--- connection.js
+++ connection.js
@@ -1,12 +1,12 @@
-// const config = require('././/config');
-require ('dotenv/config');
+const config = require('././/config');
+// require ('dotenv/config');
 const Pool = require("pg").Pool;
 // const validate= require('./validation.js');
 global.pool = new Pool({
   user: "iaqsqvmm",
   host: "satao.db.elephantsql.com",
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  database: config.DATABASE,
+  password: config.PASSWORD,
   port: 5432,
 });
 pool.connect();
