6282d71a9cee58ae80185288aa766bb9d9ae8287
Deepanshu Agrawal <deepanshu.a@lsq.net>
2022-06-02 13:22:28 +0530

    deepanshu

--- routes/language.js
+++ routes/language.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("../database.js");
+const pool = require("../connection.js");
 const router = express.Router();
 router.get("/", async (req, res) => {
   try {
