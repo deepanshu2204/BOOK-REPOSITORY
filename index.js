00531a47b34af22c84bf26f3ecf0e00969ac703a
Alok Kumar Sahu <alok.sahu@leadsquared.com>
2022-06-09 13:10:24 +0530

    alok index changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const pricing = require("./routes/pricing.js");
 const details = require("./routes/details.js");
 const authorRoutes = require("./routes/author.js");
 const stockRoutes = require("./routes/stock.js");
-const abstractRoutes = require("./routes/abstract.js");
 const department = require("./routes/department.js");
 const language = require("./routes/language.js");
 const app = express();
@@ -16,7 +15,6 @@ app.use("/pricing", pricing);
 app.use("/details", details);
 app.use("/author", authorRoutes);
 app.use("/stock", stockRoutes);
-app.use("/abstract", abstractRoutes);
 app.use("/languages", language);
 app.use("/department", department);
 app.get("/", (req, res) => {
